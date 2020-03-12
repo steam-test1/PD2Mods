8a45854b86496b243ce798a08edbb9a023ef50bd
steam-test1 <steam-test1@users.noreply.github.com>
2020-03-12 19:36:32 +0000

    clear

--- HoloInfo/lua/Unitnetworkhandler.lua
+++
@@ -1,7 +0,0 @@
-local _o_sync_teammate_progress = UnitNetworkHandler.sync_teammate_progress
-function UnitNetworkHandler:sync_teammate_progress(type_index, enabled, tweak_data_id, timer, success, sender)
-	_o_sync_teammate_progress(self, type_index, enabled, tweak_data_id, timer, success, sender)
-	if tweak_data_id == "corpse_alarm_pager" and success == true then
-		managers.hud:pager_increase()
-	end	
-end
